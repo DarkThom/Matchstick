15e3caa6f29b92f6ff7b0956bb20b617a40764d2
BLENEAU Thomas <thomas.bleneau@epitech.eu>
2017-05-10 10:27:20 +0200

    Merge fixe

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
-*
+/*
 ** main.c for  in /home/BLENEA_T/Projects/Semestre_2/CPE/CPE_2016_matchstick
 **
 ** Made by Thomas BLENEAU
 ** Login   <BLENEA_T@epitech.net>
 **
 ** Started on  Wed Feb 15 10:42:16 2017 Thomas BLENEAU
-** Last update Wed May 10 10:26:37 2017 Thomas BLENEAU
+** Last update Wed May 10 10:27:17 2017 Thomas BLENEAU
 */
 
 #include "str_to_wordtab.h"
